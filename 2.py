944203e41f1eee1fae391f4e38271043eb7a185a
Darialr <darial.ryaguzova@gmail.com>
2024-02-15 16:58:37 +0700

    for sure ready

--- 2.py
+++ 2.py
@@ -1,6 +1,6 @@
-print('Привет,Python!',
-      'Hello,Python!',
-      'Bonjour Python!',
-      'Hej,Python!',
-      'Hola,Python!',
-      sep='\n')
+print('Привет,Python!')
+print('Hello,Python!')
+print('Bonjour Python!')
+print('Hej,Python!')
+print('Hola,Python!')
+
