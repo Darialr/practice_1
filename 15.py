87f89b9c5dcbc35473ff47c66c075d1423452622
Darialr <darial.ryaguzova@gmail.com>
2024-02-15 19:58:51 +0700

    ready

---
+++ 15.py
@@ -0,0 +1,9 @@
+sm = float(input())
+inch = sm/2.54
+foot = inch/12
+yards = foot/3
+miles = yards/1760
+print(f'{yards} ярдов')
+print(f'{miles} мили')
+print(f'{foot} футов')
+print(f'{inch} дюймов')
